e77480d34488ba848ed77154be51d2efa674bcdf
Murat <63672103+MuratUseini123@users.noreply.github.com>
2021-06-12 15:12:55 +0200

    Added new faculty class and changed main part

---
+++ ConsoleApp/Faculty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp;
+
+namespace ConsoleApp
+{
+    public class Faculty
+    {
+        public List<Human> students{ get; set; }
+        public List<string> subjects { get; set; }
+
+        public Faculty()
+        {
+            this.students = new List<Human>();
+            this.subjects = new List<string>();
+        }
+
+        public void initSubjects()
+        {
+            subjects.Add("Computer Networks");
+            subjects.Add("Introduction Computer Science");
+            subjects.Add("Computer Graphics");
+            subjects.Add("Object Oriented Programming");
+        }
+
+        public void addNewStudent(Human newStudent)
+        {
+            if (newStudent != null)
+            {
+                this.students.Add(newStudent);
+            }
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,41 +6,60 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
+    public class Human
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Human()
+        {
+
+        }
+        public Human(string Name, string Surname, int Age)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Age = Age;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2}", Name, Surname, Age);
+        }
+        public static List<Human> createPeople(int num)
+        {
+            List<Human> people = new List<Human>();
+            for (int i = 0; i < num; i++)
+            {
+                people.Add(new Human("testname", "testsurname", i + 10));
+            }
+            return people;
+        }
+    }
     class Program
     {
-        public class Human
-        {
-            public int Age { get; set; }
-            public string Name { get; set; }
-            public string Surname { get; set; }
-            public Human(string Name,string Surname,int Age)
-            {
-                this.Name = Name;
-                this.Surname = Surname;
-                this.Age = Age;
-            }
-            public override string ToString()
-            {
-                return string.Format("{0} {1} {2}", Name, Surname, Age);
-            }
-            public static List<Human> createPeople(int num)
-            {
-                List<Human> people = new List<Human>();
-                for (int i = 0; i < num; i++)
-                {
-                    people.Add(new Human("testname", "testsurname", i + 10));
-                }
-                return people;
-            }
-        }
-
-        
         static void Main(string[] args)
         {
-            List<Human> people = Human.createPeople(10);
-            foreach(Human human in people)
+            Faculty testFaculty = new Faculty();
+
+            testFaculty.initSubjects();
+            for(int i = 0; i < 100; i++)
             {
-                Console.WriteLine(human.ToString());
+                testFaculty.addNewStudent(new Human()
+                {
+                    Name = "testname" + i,
+                    Surname = "testSurname" + i,
+                    Age = i + 10
+                }); 
+            }
+
+            foreach(Human i in testFaculty.students)
+            {
+                Console.WriteLine(i.ToString());
+            }
+            Console.WriteLine("----------------------");
+            foreach(var subject in testFaculty.subjects)
+            {
+                Console.WriteLine(subject);
             }
             Console.ReadKey();
         }
